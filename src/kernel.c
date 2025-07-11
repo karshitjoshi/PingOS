04cdcc3e34dbce180ad7d60e8564cd577b703fc3
Karshit Joshi <karshitkjoshi@gmail.com>
2025-07-11 18:41:38 +0530

    Cleaned

--- src/kernel.c
+++ src/kernel.c
@@ -3,7 +3,6 @@
 #include "outb_inb.c"
 #include "cursor.c"
 #include "keyboard.c"
-#include "memory_mapping.h"
 
 extern void StartIDT();
 
@@ -17,12 +16,6 @@ int _start()
     PrintStringXY("KeyBoard Input Enabled",22,0,12);
     PrintStringXY("Number Of Memory Regions Present :",34,0,13);PrintStringXY(IntegerToString(MemRegCounts),1,34,13);
     ValidMemRegs();
-    InitiateMemoryMap();
-    //heapMemory is the Heap in form of Struct which conains every information abt Heap
-    PrintStringXY((IntegerToString(heapMemory.Length)),50,0,14);
-    PrintStringXY((IntegerToString(heapMemory.Address)),50,0,15);
-    PrintStringXY((IntegerToString(heapMemory.MemoryType)),50,0,16);
-    PrintStringXY((IntegerToString(heapMemory.Attr_Types)),50,0,17);
     //Enabled Cursor and Remember Text Mode Stays in 80x25
     while(1);
 }
